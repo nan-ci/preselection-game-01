ca3d7654290cdf52ef0c4f45186b213957467913
Yoann Cribier <ycribier@student.42.fr>
2017-09-13 14:57:32 +0200

    Provider added to pass store through whole hierarchy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker'
 import store from './store'
 
 ReactDOM.render((
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 ), document.getElementById('root'))
 
 registerServiceWorker()
