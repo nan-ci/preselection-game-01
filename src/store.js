e250172ced47bf02fc76de6f0813cefd45489779
Yoann Cribier <ycribier@student.42.fr>
2017-09-16 19:58:37 +0200

    package.json - redux-logger added & used as middleware

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
+import loggerMiddleware from 'redux-logger'
 import thunk from 'redux-thunk'
 
 import reducer from './reducers'
@@ -8,7 +9,10 @@ const initialState = {}
 const store = createStore(
   reducer,
   initialState,
-  applyMiddleware(thunk),
+  applyMiddleware(
+    thunk,
+    loggerMiddleware,
+  ),
   // (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),
 )
 
